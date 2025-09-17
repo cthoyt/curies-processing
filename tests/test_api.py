3fc63ad6d85fa99eb5af7345918d29692a7d72f4
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-09-17 10:58:38 +0200

    Add SMID postprocessing (#2)
    In ChEBI, SMID references often come with this extra character at the
    end
    
    https://smid-db.org/detail/ascr%2321

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,6 +14,7 @@ class TestAPI(unittest.TestCase):
         """Test wrapping a converter."""
         c = curies.get_obo_converter()
         c.add_prefix("NLXDYS", "http://uri.neuinfo.org/nif/nifstd/nlx_dys_")
+        c.add_prefix("smid", "https://smid-db.org/detail/")
         c = curies_processing.wrap(c)
 
         r1 = c.parse("GOC:CJM", strict=True)
@@ -23,3 +24,8 @@ class TestAPI(unittest.TestCase):
         r2 = c.parse("NIFSTD:nlx_dys_20090602", strict=True)
         self.assertEqual("NLXDYS", r2.prefix)
         self.assertEqual("20090602", r2.identifier)
+
+        # test processing suffix
+        r3 = c.parse("smid:bhos%2322%0D")
+        self.assertEqual("smid", r3.prefix)
+        self.assertEqual("bhos%2322", r3.identifier)
